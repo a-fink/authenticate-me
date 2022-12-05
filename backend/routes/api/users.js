7b96838b9a4c26a98e3ebb56cfec032ba98fa0bd
Aubrey <aubreyfinkelstein@gmail.com>
2022-12-05 16:43:09 -0700

    created and connected validation middleware for the body of a signup request in /api/users, commented out the hello world test route in routes/index.js

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,11 +3,26 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
-// router for all routes beginning with /api/users
+// router for all routes beginning with /api/users (already handled by routers above this one, so just / in relation this router)
 
-// route for creating a new user - will be a POST request to the /api/users route (already handled by routers above this one, so just / in relation this router)
+// middleware to validate information coming in on the request body for creating a user
+// using built in check function from express-validator, and the error handler for validation errors we built
+// check for given conditions, and respond with given error message if fails, then send results to the handleValidationErrors middleware
+const validateSignup = [
+    check('email').exists({checkFalsy: true}).isEmail().withMessage('Please provide a valid email'),
+    check('username').exists({checkFalsy: true}).isLength({min: 4}).withMessage('Please provide a username with at least 4 characters.'),
+    check('username').not().isEmail().withMessage('Username cannot be an email.'),
+    check('password').exists({checkFalsy: true}).isLength({min: 6}).withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+// route for creating a new user - will be a POST request
+// adding validateSignup middleware as a parameter here to connect the middleware
 router.post('/',
+    validateSignup,
     asyncHandler(async (req, res) => {
         // destructure the user information from the request body
         const { email, password, username } = req.body;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,11 +7,14 @@ const apiRouter = require('./api');
 // connect the apiRouter
 router.use('/api', apiRouter);
 
+module.exports = router;
+
+/*
+TEST ROUTE TO MAKE SURE SERVER UP - ALL WORKING NOW - KEEPING IN CASE NEED LATER FOR MORE TESTING
 // create a test route
 // set a cookie on the response with a name of XSRF-TOKEN and give it the value of the method we set up wtih csrf middleware before
 router.get('/hello/world', function(req, res){
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
 });
-
-module.exports = router;
+*/
