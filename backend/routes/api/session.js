0d2249e65806d93ead6e367f84c1832f5967ac6b
Aubrey <aubreyfinkelstein@gmail.com>
2022-12-05 16:12:30 -0700

    added restoreUser route under /api/session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
-const { setTokenCookie, restroreUser } = require('../../utils/auth');
+const { setTokenCookie, restroreUser, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-// router for all routes beginning with /api/session
+// router for all routes beginning with /api/session (already handled by routers above this one, so just / in relation this router)
 
-// route used for logging in - will be POST request to /api/session (already handled by routers above this one, so just / in relation this router)
+// route used to get a session - will be a GET request
+// connect the restoreUser middleware which checks for a user session token and puts the corresponding user on the req object
+router.get('/', restoreUser, (req, res) => {
+    // attempt to destructure the user from the req object
+    const { user } = req;
+    // if the user exist return the user in json format
+    if(user){
+        return res.json({user: user.toSafeObject()});
+    }
+    // if user doesn't exist return an empty object in json format
+    else{
+        return res.json({});
+    }
+});
+
+// route used for logging in - will be POST request
 router.post('/',
     asyncHandler(async (req, res, next) => {
         // destructure the credential & password from the request body
@@ -32,7 +47,7 @@ router.post('/',
     })
 );
 
-// route used for logging out - will be DELETE request to /api/session (already handled by routers above this one, so just / in relation this router)
+// route used for logging out - will be DELETE request
 router.delete('/', (_req, res) => {
     // when a user logs out we want to remove the session token cookie and then send a success response
     res.clearCookie('token');
