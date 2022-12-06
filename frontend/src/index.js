ba1641b2b8a82ce550ca1e073b6d4fafb5e3f45d
Aubrey <aubreyfinkelstein@gmail.com>
2022-12-06 12:27:51 -0700

    created and configured redux store, connected redux store and react router in frontend/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// in development only, expose the store on window -> used for testing redux store setup is correctly connected
+if(process.env.NODE_ENV !== 'production'){
+  window.store = store;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
